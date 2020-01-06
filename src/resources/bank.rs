e812e404338d803ccf333a8c1c0617c679abb9fe
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2020-01-06 13:40:24 +0100

    Bank loading code is working

--- src/resources/bank.rs
+++ src/resources/bank.rs
@@ -13,140 +13,193 @@ pub fn load_bank_entry(bank_path: &Path, entry: &MemEntry) -> io::Result<Vec<u8>
     fd.seek(io::SeekFrom::Start(entry.bank_offset.into()))?;
     let mut ret = vec![0;entry.size.into()];
     if entry.packed_size != entry.size {
-        let mut packed_buff = vec![0;entry.packed_size.into()];
-        fd.read(&mut packed_buff)?;
-        UnpackContext::unpack(&packed_buff, &mut ret)?;
+        let mut bank_entry_buff = vec![0;entry.packed_size.into()];
+        fd.read(&mut bank_entry_buff)?;
+        unpack(&bank_entry_buff, &mut ret)?;
     } else {
         fd.read(&mut ret)?;
     }
     Ok(ret)
 }
 
-struct UnpackContext<'a, 'b> {
-    size: u16,
-    datasize: u32,
-    crc: u32,
-    chk: u32,
-
-    src: &'a[u8],
-    src_pos: usize,
-    dst: &'b mut [u8],
-    dst_pos: usize,
+fn to_u32(buff: &[u8], offset: usize) -> u32 {
+    u32::from_be_bytes(
+        buff[offset..offset + 4].try_into().unwrap()
+    )
 }
 
-impl<'a, 'b> UnpackContext<'a, 'b> {
+struct BitStream<'a> {
+    data: &'a[u8],
+    current_word_pos: usize,
+    current_word: u32,
+    current_bit_shift: usize,
+    current_word_is_first: bool,
+}
 
-    fn get_u32(&mut self) -> u32 {
-        self.src_pos -= 4;
-        u32::from_be_bytes(self.src[self.src_pos..self.src_pos+4].try_into().unwrap())
+impl<'a> BitStream<'a> {
+    fn new(data: &'a[u8]) -> Self {
+        let current_word_pos = data.len() - 4;
+        let current_word = to_u32(data, current_word_pos);
+        BitStream{
+            data,
+            current_word_pos,
+            current_word,
+            current_bit_shift: 0,
+            current_word_is_first: true,
+        }
     }
 
-    fn unpack(src: &'a[u8], dst: &'b mut [u8]) -> io::Result<()>{
-        let mut ctx = Self{
-            size: 0,
-            datasize: 0,
-            crc: 0,
-            chk: 0,
-            src: src,
-            src_pos: src.len(),
-            dst: dst,
-            dst_pos: 0,
-        };
+    fn get_bit(&mut self) -> io::Result<u8> {
+        Ok(self.next().unwrap())
+    }
 
-        ctx.datasize = ctx.get_u32();
-        ctx.crc = ctx.get_u32();
-        ctx.chk = ctx.get_u32();
-        ctx.dst_pos = (ctx.datasize - 1) as usize;
+    fn get_bits(&mut self, count: usize) -> io::Result<u32> {
+        let mut bits: u32 = 0;
+        for _ in 0..count {
+            bits <<= 1;
+            bits |= self.get_bit()? as u32;
+        }
+        Ok(bits)
+    }
 
-        ctx.crc ^= ctx.chk;
-        loop {
-            if !ctx.next_chunk() {
-                ctx.size = 1;
-                if !ctx.next_chunk() {
-                    ctx.dec_unk1(3, 0);
-                } else {
-                    ctx.dec_unk2(8);
-                }
+}
+
+impl<'a> Iterator for BitStream<'a> {
+    type Item = u8;
+
+    fn next(&mut self) -> Option<u8> {
+        // First byte decoding is broken: switch to next bytes as soon as
+        // the value is 1 instead of reading each of the 32 bits.
+        // This is most likely a flaw in the original implementation: each word
+        // is shifted to the rigt and bitmaked with 1 << 31 in order to know
+        // when it is entirely consumed. However the first word is loaded
+        // is different place where the bitmask is not applied.
+        if self.current_word_is_first && (self.current_word >> self.current_bit_shift) <= 1 {
+            self.current_word_is_first = false;
+            self.current_bit_shift = 32;
+        }
+
+        if self.current_bit_shift == 32 {
+            self.current_bit_shift = 0;
+            self.current_word_pos -= 4;
+            self.current_word = to_u32(self.data, self.current_word_pos);
+        }
+
+        let bit = ((self.current_word >> self.current_bit_shift) & 1) as u8;
+        self.current_bit_shift += 1;
+
+        Some(bit)
+    }
+}
+
+struct BuffWriter<'a> {
+    buff: &'a mut [u8],
+    pos: usize,
+}
+
+impl<'a> BuffWriter<'a> {
+    fn new(buff: &'a mut [u8]) -> Self {
+        Self {
+            pos: buff.len(),
+            buff: buff,
+        }
+    }
+
+    fn copy_bytes(&mut self, size: u32, bs: &mut BitStream) -> io::Result<()>{
+        for _ in 0..size {
+            self.pos -= 1;
+            self.buff[self.pos] = bs.get_bits(8)? as u8;
+        }
+        Ok(())
+    }
+
+    fn duplicate_bytes(&mut self, size: u32, offset: u32) {
+        for _ in 0..size {
+            self.pos -= 1;
+            self.buff[self.pos] = self.buff[self.pos + offset as usize];
+        }
+    }
+}
+
+fn unpack(src: &[u8], dst: & mut [u8]) -> io::Result<()> {
+    // Sanity checks
+    if src.len() <= 8 {
+        return Err(io::Error::new(io::ErrorKind::Other, "Packed size must be > 8"));
+    }
+    if src.len() % 4 != 0 {
+        return Err(io::Error::new(io::ErrorKind::Other, "Packed size must be a multiple of 4"));
+    }
+    let data_size_pos = src.len() - 4;
+    let crc_pos = data_size_pos - 4;
+    let data_end = crc_pos;
+    let data_size = to_u32(src, data_size_pos);
+    let expected_crc = to_u32(src, crc_pos);
+    let mut bs = BitStream::new(&src[..data_end]);
+
+    // Check unpacked size
+    if data_size > dst.len().try_into().unwrap() {
+        return Err(io::Error::new(io::ErrorKind::Other, "Unpacked size too big for output buffer"));
+    }
+
+    // Check CRC
+    let mut crc: u32 = expected_crc;
+    for i in (0..data_end).step_by(4) {
+        crc ^= to_u32(src, i);
+    }
+    if crc != 0 {
+        return Err(io::Error::new(io::ErrorKind::Other, "Bad CRC check"))
+    }
+
+    // All good, now let's do the unpacking !
+    let mut dst_cursor = BuffWriter::new(dst);
+    let mut unpacked_size: u32 = 0;
+    let mut size: u32;
+    while unpacked_size < data_size {
+        if bs.get_bit()? == 0 {
+            if bs.get_bit()? == 0 {
+                // copy between 1 and 8 bytes
+                size = bs.get_bits(3)? + 1;
+                dst_cursor.copy_bytes(size, &mut bs)?;
             } else {
-                let c: u16 = ctx.get_code(2);
-                if c == 3 {
-                    ctx.dec_unk1(8, 8);
-                } else {
-                    if c < 2 {
-                        ctx.size = c + 2;
-                        ctx.dec_unk2((c + 9).try_into().unwrap());  // TODO: investigate this...
-                    } else {
-                        ctx.size = ctx.get_code(8);
-                        ctx.dec_unk2(12);
-                    }
+                // duplicate 2 bytes
+                size = 2;
+                let offset = bs.get_bits(8)?;
+                dst_cursor.duplicate_bytes(size, offset);
+            }
+        } else {
+            let c = bs.get_bits(2)?;
+            match c {
+                0 => {
+                    // duplicate 3 bytes
+                    size = 3;
+                    let offset = bs.get_bits(9)?;
+                    dst_cursor.duplicate_bytes(size, offset);
+                }
+                1 => {
+                    // duplicate 4 bytes
+                    size = 4;
+                    let offset = bs.get_bits(10)?;
+                    dst_cursor.duplicate_bytes(size, offset);
+                }
+                2 => {
+                    // duplicate between 1 and 256 bytes
+                    size = bs.get_bits(8)? + 1;
+                    let offset = bs.get_bits(12)?;
+                    dst_cursor.duplicate_bytes(size, offset);
+                }
+                _ => { // c == 3
+                    // copy between 9 and 264 bytes
+                    size = bs.get_bits(8)? + 1 + 8;
+                    dst_cursor.copy_bytes(size, &mut bs)?;
                 }
             }
-            if ctx.datasize <= 0 {
-                break;
-            }
         }
-
-        if ctx.crc == 0 {
-            Ok(())
-        } else {
-            Err(io::Error::new(io::ErrorKind::Other, "Bad CRC check"))
-        }
+        unpacked_size += size;
     }
 
-    fn dec_unk1(&mut self, num_chunks: u8, add_count: u8) {
-        let mut count: u16 = self.get_code(num_chunks) + add_count as u16 + 1;
-        self.datasize -= count as u32;
-        while count > 0 {
-            count -= 1;
-            assert!(self.dst_pos >= self.src_pos);
-            self.dst[self.dst_pos] = self.get_code(8) as u8;
-            self.dst_pos -= 1;
-        }
+    if unpacked_size != data_size {
+        return Err(io::Error::new(io::ErrorKind::Other, "Final unpacked size doesn't mach with expected unpacked size"))
     }
 
-    fn dec_unk2(&mut self, num_chunks: u8) {
-        let i: u16 = self.get_code(num_chunks);
-        let mut count: u16 = self.size + 1;
-        // debug(DBG_BANK, "Bank::decUnk2(%d) i=%d count=%d", num_chunks, i, count);
-        self.datasize -= count as u32;
-        while count > 0 {
-            count -= 1;
-            // assert(_oBuf >= _iBuf && _oBuf >= _startBuf);
-            self.dst[self.dst_pos] = self.dst[self.dst_pos + i as usize];
-            self.dst_pos -= 1;
-        }
-    }
-
-    fn get_code(&mut self, mut num_chunks: u8) -> u16 {
-        let mut c: u16 = 0;
-        while num_chunks > 0 {
-            num_chunks -= 1;
-            c <<= 1;
-            if self.next_chunk() {
-                c |= 1;
-            }           
-        }
-        c
-    }
-
-    fn next_chunk(&mut self) -> bool {
-        let mut cf = self.rcr(false);
-        if self.chk == 0 {
-            // assert(_iBuf >= _startBuf);
-            self.chk = self.get_u32();
-            self.crc ^= self.chk;
-            cf = self.rcr(true);
-        }
-        cf
-    }
-
-    fn rcr(&mut self, cf: bool) -> bool {
-        let rcf: bool = (self.chk & 1) == 1;
-        self.chk >>= 1;
-        if cf {
-            self.chk |= 0x80000000;
-        }
-        rcf
-    }
-
+    Ok(())
 }
--- src/resources/mod.rs
+++ src/resources/mod.rs
@@ -13,7 +13,7 @@ pub fn load_resources(resources_path: &Path) -> io::Result<Resources> {
     for entry in entries {
     	println!("LOADING BANK{:02X} 0x{:X} -> 0x{:X}", entry.bank_id, entry.bank_offset, entry.bank_offset + entry.packed_size as u32);
         let entry_path = resources_path.join(format!("BANK{:02X}", entry.bank_id));
-        let bank_buff = bank::load_bank_entry(&entry_path, &entry);
+        let bank_buff = bank::load_bank_entry(&entry_path, &entry)?;
         // println!("~~~~~~~~~{:?}", bank_buff);
     }
     return Ok(Resources{});
